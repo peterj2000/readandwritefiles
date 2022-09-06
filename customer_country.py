3c852d6e1ac2a778bf563134332159dd9824346c
peter <pj@test.com>
2022-09-06 13:05:14 -0500

    Comp Customer_country

--- customer_country.py
+++ customer_country.py
@@ -1,4 +1,5 @@
 import csv
+
 infile = open("customers.csv" , "r")
 outfile = open('customer_country.csv' , "w")
 
@@ -6,7 +7,11 @@ csvfile = csv.reader(infile, delimiter=',')
 
 next(csvfile) #this will skip the first row
 
-for record in csvfile:
-    print("Name:" + record[1] +''+ record[2] + ',' + record[4])
+outfile.write("Full name, country\n")
 
-    input()
+for record in csvfile:
+    full_name = record[1] + " " + record[2]
+    country = record[4]
+    outfile.write(full_name + "," + country + '\n')
+
+outfile.close()
