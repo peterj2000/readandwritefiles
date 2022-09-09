e186ae87ba5eab2a51a55cda66db13d01d7c57ce
peter <pj@test.com>
2022-09-08 22:00:40 -0500

    avg steps update

--- avg_steps.py
+++ avg_steps.py
@@ -2,3 +2,29 @@ import csv
 
 infile = open("steps.csv", 'r')
 outfile = open('avg_steps.csv',"w")
+
+csvfile = csv.reader(infile, delimiter=',')
+
+next(csvfile)
+
+
+prof_b = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+count = 1
+
+outfile.write('Month, Avg Steps Taken\n')
+for x in csvfile:
+    #print(i)
+    if float(x[0])!=count:
+        count-=1
+        avg=(a/i)
+        avg=format(avg,',.2f')
+        print(prof_b[count]+', '+str(avg))
+        outfile.write(prof_b[count]+', '+str(avg)+'\n')
+        count+=2
+        i=0
+        a=0
+    if float(x[0])==count:
+        a+=float(x[1])
+        i+=1
+
+
