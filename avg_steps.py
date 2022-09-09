f4adccf34df946674f6f9fba62ede2fe0eb384a7
peter <pj@test.com>
2022-09-08 23:41:26 -0500

    update

--- avg_steps.py
+++ avg_steps.py
@@ -8,18 +8,18 @@ csvfile = csv.reader(infile, delimiter=',')
 next(csvfile)
 
 
-prof_b = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+mon = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 count = 1
 
-outfile.write('Month, Avg Steps Taken\n')
+outfile.write('Month, Average Steps Taken\n')
 for x in csvfile:
     #print(i)
     if float(x[0])!=count:
         count-=1
-        avg=(a/i)
-        avg=format(avg,',.2f')
-        print(prof_b[count]+', '+str(avg))
-        outfile.write(prof_b[count]+', '+str(avg)+'\n')
+        average =(a/i)
+        average=format(average,',.2f')
+        print(mon[count]+', '+str(average))
+        outfile.write(mon[count]+', '+str(average)+'\n')
         count+=2
         i=0
         a=0
